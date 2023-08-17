e28e3429e41feb6aaf1da4ba852a03c344289efa
AditiInito <aditi.choudhary@inito.com>
2023-08-17 16:21:51 +0530

    User and categories added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,25 @@
 class Article < ApplicationRecord
+    before_validation do
+        puts 'before validation'
+    end
+    after_validation do
+        puts 'after validation'
+    end
+    before_save do
+        puts 'before save'
+    end
+    before_create do
+        puts 'before create'
+    end
+    after_create do
+        puts 'after create'
+    end
+    after_save do
+        puts 'after save'
+    end
+    belongs_to :user
+    has_many :article_categories
+    has_many :categories, through: :article_categories
     validates :title, presence: true, length: {minimum: 6, maximum: 50}
     validates :description, presence: true, length: {minimum: 10, maximum: 500}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do
+    get 'preview', on: :new
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root('pages#home')
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources 'categories', except: [:destroy]
 end
